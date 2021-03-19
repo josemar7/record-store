d8f6f76cbf40057972aa10ce9dbca3d4a7c7083f
Pepo <jmmartinezsan@gmail.com>
2021-03-19 07:41:27 +0100

    styles with USER role

--- src/main/java/org/pepo/record/controller/StyleController.java
+++ src/main/java/org/pepo/record/controller/StyleController.java
@@ -6,6 +6,7 @@ import org.pepo.record.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class StyleController {
     @Autowired
     private StyleService styleService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping(path="/all")
     public ResponseEntity<Iterable<StyleDto>> getAllArtists() {
         return ResponseUtils.createResponse(styleService.findAll(), HttpStatus.OK);
