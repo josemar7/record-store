b778425de6bc89c58bfa01af702617577c524e63
Pepo <jmmartinezsan@gmail.com>
2021-05-11 14:11:29 +0200

    when saving orders updating units record

--- src/main/java/org/pepo/record/service/orders/OrdersServiceImpl.java
+++ src/main/java/org/pepo/record/service/orders/OrdersServiceImpl.java
@@ -6,11 +6,13 @@ import org.pepo.record.SwaggerCodgen.model.OrderResponseOpenApi;
 import org.pepo.record.entity.Orders;
 import org.pepo.record.mapper.OrdersEntityOpenApiMapper;
 import org.pepo.record.repository.orders.OrdersRepository;
+import org.pepo.record.repository.record.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -26,10 +28,20 @@ public class OrdersServiceImpl implements OrdersService {
     @Autowired
     private final OrdersEntityOpenApiMapper ordersEntityOpenApiMapper;
 
+    @Autowired
+    private final RecordRepository recordRepository;
+
+    @Transactional
     @Override
     public OrderResponseOpenApi save(final Orders orders) {
         orders.setDate(LocalDateTime.now());
-        orders.getOrdersLines().get(0).setOrders(orders);
+        orders.getOrdersLines().forEach(ordersLine -> {
+            ordersLine.setOrders(orders);
+            recordRepository.findById(ordersLine.getRecord().getId()).ifPresent(record -> {
+                record.setUnits(record.getUnits() - ordersLine.getUnits());
+                recordRepository.save(record);
+            });
+        });
         return ordersEntityOpenApiMapper.ordersToOrderResponseOpenApi(ordersRepository.save(orders));
     }
 
